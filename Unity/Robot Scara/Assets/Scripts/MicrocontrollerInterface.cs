a3e5be1960ba9128942b9f2a54c3e19c9d79b2df
MARIANOCEREDA <gmarianocereda@gmail.com>
2023-01-04 23:22:19 +0100

    Unity first commit
    Unity commit

---
+++ Unity/Robot Scara/Assets/Scripts/MicrocontrollerInterface.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+/*
+ This module manages the interface between the Microcontroller and
+the Unity VR model.
+
+author: Mariano Cereda and Facundo Cardenas
+ */
+
+public class MicrocontrollerInterface : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+public class BluetoothManager
+{
+    /* BluetoothManager
+     
+    This class manages the bluetooth communication via a serial port.
+    The .NET built-in class SerialPort is used.
+    SerialPort Doumentation: https://learn.microsoft.com/en-us/dotnet/api/system.io.ports.serialport?view=dotnet-plat-ext-7.0
+    Usage example: https://www.c-sharpcorner.com/UploadFile/eclipsed4utoo/communicating-with-serial-port-in-C-Sharp/
+
+     */
+
+    private int BAUDRATE = 9600;
+    private int READ_TIMEOUT = 100;
+    private int WRITE_TIMEOUT = 100;
+    private string port = "COM05";
+    private string deviceName = "btDevice";
+    private SerialPort _serialPort;
+
+    void Connect()
+    {
+        try
+        {
+            _serialPort = new SerialPort(port, BAUDRATE, Parity.None, 8, StopBits.One);
+
+            if (!_serialPort.IsOpen)
+            {
+                _serialPort.Open();
+                setConfiguration();
+            }
+        }
+        catch (Exception err)
+        {
+            Debug.Log("Error when trying to Open Serial Communication: " + err);
+            throw err;
+        }
+
+    }
+    void Disconnect()
+    {
+        try
+        {
+            if (_serialPort.IsOpen)
+            {
+                _serialPort.Close();
+            }
+            else
+            {
+                Debug.Log("The connection is not open.");
+            }
+
+        }
+        catch (IOException err)
+        {
+            Debug.Log("Error when trying to close the connection: " + err);
+        }
+    }
+
+    void setConfiguration()
+    {
+        _serialPort.ReadTimeout = READ_TIMEOUT;
+        _serialPort.WriteTimeout = WRITE_TIMEOUT;
+        _serialPort.Handshake = Handshake.None;
+    }
+
+}   
+    }
+}
