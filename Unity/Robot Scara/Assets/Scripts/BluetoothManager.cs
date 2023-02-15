43f1c4aeb8ec2dea17efc8739ef222d9836e77c1
MARIANOCEREDA <gmarianocereda@gmail.com>
2023-02-15 20:26:08 +0100

    robot working with serial data

--- Unity/Robot Scara/Assets/Scripts/BluetoothManager.cs
+++ Unity/Robot Scara/Assets/Scripts/BluetoothManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO.Ports;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BluetoothManager : MonoBehaviour
 {
@@ -15,20 +16,23 @@ public class BluetoothManager : MonoBehaviour
     Usage example: https://www.c-sharpcorner.com/UploadFile/eclipsed4utoo/communicating-with-serial-port-in-C-Sharp/
 
      */
-
+    public GameObject robot;
     private int BAUDRATE = 115200;
     private int READ_TIMEOUT = 10000;
     private int WRITE_TIMEOUT = 10000;
     private string port = "COM3";
-    private string deviceName = "btDevice";
+    private string deviceName = "HC-06";
     private SerialPort _serialPort;
     string message;
+    public float[] speeds;
+    float[] inAngles = new float[3];
 
-    void Start()
+    private void Start()
     {
-        Connect();
+        OnConnect();
     }
-    
+
+
     void Update()
     {
         try
@@ -38,15 +42,54 @@ public class BluetoothManager : MonoBehaviour
                 message = _serialPort.ReadLine();
                 _serialPort.BaseStream.Flush();
                 Debug.Log("Message from Serial: " + message + "\n");
+                parseIncomingMessage(message);
             }
         }
         catch(Exception e) 
         {
             Debug.LogError("Error when trying to read from Serial Port: "+ port + " - " + e);
         }
+
+        ScaraController scaraController = robot.GetComponent<ScaraController>();
+        for (int i = 0; i < scaraController.joints.Length; i++)
+        {
+            SetJointSpeed(scaraController.joints[i].robotPart, speeds[i]);
+            if (inAngles[i] != 0)
+            {
+                MovementDirection directionArm = GetRotationDirection(inAngles[i]);
+                scaraController.MoveJoint(i, directionArm);
+            }
+            else
+            {
+                scaraController.MoveJoint(i, MovementDirection.None);
+            }
+        }
+        return;
     }
 
-    void Connect()
+    void SetJointSpeed(GameObject robotPart, float speed)
+    {
+        ArticulationJointController jointController = robotPart.GetComponent<ArticulationJointController>();
+        jointController.SetArticulationSpeed(speed);
+    }
+
+    static MovementDirection GetRotationDirection(float inputVal)
+    {
+        if (inputVal > 0)
+        {
+            return MovementDirection.Positive;
+        }
+        else if (inputVal < 0)
+        {
+            return MovementDirection.Negative;
+        }
+        else
+        {
+            return MovementDirection.None;
+        }
+    }
+
+    public void OnConnect()
     {
         try
         {
@@ -58,16 +101,15 @@ public class BluetoothManager : MonoBehaviour
                 _serialPort.Open();
                 setConfiguration();
             }
-            Debug.Log("Connected to Serial Port�: " + port );
+            Debug.Log("Connected to Serial Port�: " + port);
         }
         catch (Exception err)
         {
             Debug.Log("Error when trying to Open Serial Communication: " + err);
             throw err;
         }
-
     }
-    void Disconnect()
+    public void OnDisconnect()
     {
         try
         {
@@ -92,5 +134,18 @@ public class BluetoothManager : MonoBehaviour
         _serialPort.ReadTimeout = READ_TIMEOUT;
         _serialPort.WriteTimeout = WRITE_TIMEOUT;
         _serialPort.Handshake = Handshake.None;
+    }
+
+    void parseIncomingMessage(string rawMessage)
+    {
+        string[] angleValues = rawMessage.Split(";");
+        if (rawMessage != null)
+        {
+            inAngles[0] = float.Parse(angleValues[0]);
+            inAngles[1] = float.Parse(angleValues[1]);
+            inAngles[2] = float.Parse(angleValues[2]);
+
+            Debug.Log("Angles {" + inAngles[0] + " ; " + inAngles[1] + " ; " + inAngles[2] + "}");
+        }
     }
 }
